f3edff2ed9271d0cdd7b9c5482dd8e55541b57f1
casper-hansen <repsac2002@gmail.com>
2021-03-11 13:10:21 +0100

    does it work?? idk

--- rsa.rb
+++ rsa.rb
@@ -1,11 +1,11 @@
 def find_d(m,e)
-    x += 1
+    x = m + 1
     while (x*i)/e) != (x*i/e).to_i
     
         i +=1
         
     end
-    d = ((m*i)+1)/e
+    d = ((x*i))/e
     p d
     return d
 end
