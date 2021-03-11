7b9e68db847eee59691c44fd8e1c83323aed461e
casper-hansen <repsac2002@gmail.com>
2021-03-11 14:07:52 +0100

    uwuw pösh

--- rsa.rb
+++ rsa.rb
@@ -1,26 +1,35 @@
-
-
-puts "give me your first prime"
-p = gets.chomp.to_i
-
-puts "give me your second prime"
-q = gets.chomp.to_i
-
-n = p*q
-m = (p-1)*(q-1)
-
-
-
-
 def find_d(m,e)
     i = 1
-    
     while ((m*i)+1)/e != (((m*i)+1)/e).to_i
-        p "i is #{i}"
         i +=1
-        
     end
     d = ((m*i)+1)/e
-    p d
     return d
 end
+
+
+many_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+# 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271
+p = many_numbers[rand(0..(many_numbers.length - 1))]
+q =  many_numbers[rand(0..(many_numbers.length - 1))]
+
+
+
+n = p*q
+m = ((p-1)*(q-1)).to_f
+e = 1.0
+
+while m/e == (m/e).to_i
+    e += 1.0
+end
+
+p "n is #{n}"
+p "e is #{e}"
+
+d = find_d(m,e)
+
+p "give me your message"
+message = gets.chomp.to_i
+
+actual_message = (message**d)%n
+p "the message was" + " #{actual_message}"
