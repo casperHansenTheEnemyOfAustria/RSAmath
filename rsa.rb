92139e018bc771d3dd76c8753281cf64421f9f3a
casper-hansen <repsac2002@gmail.com>
2021-03-11 13:23:15 +0100

    made it actually work

--- rsa.rb
+++ rsa.rb
@@ -1,12 +1,14 @@
 def find_d(m,e)
-    x = m + 1
-    while (x*i)/e) != (x*i/e).to_i
+    i = 1
     
+    while ((m*i)+1)/e != (((m*i)+1)/e).to_i
+        p "i is #{i}"
         i +=1
         
     end
-    d = ((x*i))/e
+    d = ((m*i)+1)/e
     p d
     return d
 end
 
+find_d(216.0, 5.0)
